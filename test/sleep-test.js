97fdad5594a9f536dabdf66ebc124da98cacbb97
Kayla Wood <kaylaewood@gmail.com>
2019-10-20 20:25:59 -0600

    Rewrite Sleep-Repository tests to account for refactored functions

--- test/sleep-test.js
+++ test/sleep-test.js
@@ -9,33 +9,33 @@ describe('Sleep', () => {
   beforeEach(() => {
     sleep = new Sleep(sleepTestData);
   });
-  
+
   it('should be an instance of Sleep', () => {
     expect(sleep).to.be.an.instanceOf(Sleep);
   });
 
   it('should return a users average amount of sleep', () => {
-    expect(sleep.returnAverageSleep(1)).to.equal(8.3);
+    expect(sleep.returnAverageSleepInfo(1, 'hoursSlept')).to.equal(8.3);
   });
 
   it('should return a users average quality of sleep', () => {
-    expect(sleep.returnAverageSleepQuality(1)).to.equal(2.7);
+    expect(sleep.returnAverageSleepInfo(1, 'sleepQuality')).to.equal(2.7);
   });
 
   it('should return how much a user slept on a particular day', () => {
-    expect(sleep.returnAmountSlept(1, '2019/06/15')).to.equal(6.1);
+    expect(sleep.returnSleepInfo(1, '2019/06/15', 'hoursSlept')).to.equal(6.1);
   });
 
   it('should return the quality of sleep for a particular day', () => {
-    expect(sleep.returnSleepQuality(1, '2019/06/16')).to.equal(3.8);
+    expect(sleep.returnSleepInfo(1, '2019/06/16', 'sleepQuality')).to.equal(3.8);
   })
 
   it('should show how much a user slept per day for a given week', () => {
-    expect(sleep.returnSleepByWeek(3, '2019/06/21')).to.eql([10.8, 10.7, 5.3, 9.8, 7.2, 9.4, 8.9]);
+    expect(sleep.returnSleepInfoByWeek(3, '2019/06/21', 'hoursSlept')).to.eql([10.8, 10.7, 5.3, 9.8, 7.2, 9.4, 8.9]);
   });
 
   it('should show a users quality of sleep per day for a given week', () => {
-    expect(sleep.returnSleepQualityByWeek(3, '2019/06/22').length).to.equal(7);
+    expect(sleep.returnSleepInfoByWeek(3, '2019/06/22', 'sleepQuality').length).to.equal(7);
   });
 
   it('should return the average sleep quality for all users', () => {
